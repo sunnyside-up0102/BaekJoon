7755388b19b417cce2eba8daad1a0c0ba5d83fa2
sunnyside-up0102 <87693330+sunnyside-up0102@users.noreply.github.com>
2022-07-25 11:29:39 +0900

    0725

--- src/Baek2292_벌집.java
+++ src/Baek2292_벌집.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class Baek2292_¹úÁý {
 
 	public static void main(String[] args) {
 		
-	}
-
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		int count = 1;
+		int first = 2;
+		
+		if(N==1)
+			System.out.println("1");
+		
+		while(first<=N) {
+			first = first + (6*count);
+			count++;
+		}
+	
+	System.out.println(count);
+}
 }
